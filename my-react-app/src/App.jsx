eab0032be22c032d11ac4a5e9bc603999425a25f
candykishorekumar <candykishorekumar@gmail.com>
2026-06-15 14:56:53 +0530

    second commit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,17 @@
 import Dashboard from "./components/Dashboard";
 import { BrowserRouter, Routes, Route,  } from 'react-router-dom';
- function App() {
+import {PieChart} from "recharts";
+import {BarChart} from "recharts";
+import Footer from "./components/Footer";
+function App() {
   return(
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path='/Dashboard'  element={<Dashboard/>}> </Route>
+          <Route path='/PieChart'  element={<PieChart/>}> </Route>
+          <Route path='/BarChart'  element={<BarChart/>}> </Route>
+          <Route path='/Footer'  element={<Footer/>}> </Route>
         </Routes>
       </BrowserRouter>
 
